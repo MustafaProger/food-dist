c81928d0b2dda298818f6c3ddb030165de1f44e2
DIOR <diertojiev@yandex.ru>
2024-07-28 17:15:50 +0300

    Создаем навигацию для слайдов

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,6 @@
 // Урок 91. Создаем слайдер на сайте, вариант 1
 
+
 window.addEventListener('DOMContentLoaded', () => {
     const images = document.querySelectorAll('.offer__slide'),
         prev = document.querySelector('.offer__slider-prev'),
@@ -7,52 +8,95 @@ window.addEventListener('DOMContentLoaded', () => {
         current = document.querySelector('#current'),
         slidesWrapper = document.querySelector('.offer__slider-wrapper'),
         slidersField = document.querySelector('.offer_slider-inner'),
-        width = window.getComputedStyle(slidesWrapper).width;
+        width = window.getComputedStyle(slidesWrapper).width,
+        slider = document.querySelector('.offer__slider');
 
     let offset = 0;
+    let slideIndex = 1;
 
     slidersField.style.width = 100 * images.length + '%';
-
     slidersField.style.display = 'flex';
     slidersField.style.transition = '0.5s all';
 
-    slidesWrapper.style.overflow = 'hidden'
+    slidesWrapper.style.overflow = 'hidden';
 
     images.forEach(img => {
         img.style.width = width;
     });
 
-    next.addEventListener('click', () => {
+    function updateCurrentSlideNumber() {
+        if (slideIndex < 10) {
+            current.innerHTML = `0${slideIndex}`;
+        } else {
+            current.innerHTML = slideIndex;
+        }
+    }
 
+    function moveToSlide(index) {
+        offset = +width.slice(0, width.length - 2) * (index - 1);
+        slidersField.style.transform = `translateX(-${offset}px)`;
+        slideIndex = index;
+        updateCurrentSlideNumber();
+        activeDot();
+    }
+
+    next.addEventListener('click', () => {
         if (offset == +width.slice(0, width.length - 2) * (images.length - 1)) {
             offset = 0;
+            slideIndex = 1;
         } else {
-            offset += +width.slice(0, width.length - 2)
+            offset += +width.slice(0, width.length - 2);
+            slideIndex++;
         }
-
-        if (current.innerHTML == `0${images.length}`) {
-            current.innerHTML = `01`
-        } else {
-            current.innerHTML = `0${+current.innerHTML + 1}`
-        }
-
-        slidersField.style.transform = `translateX(-${offset}px)`
+        updateCurrentSlideNumber();
+        activeDot();
+        slidersField.style.transform = `translateX(-${offset}px)`;
     });
 
     prev.addEventListener('click', () => {
-
         if (offset == 0) {
             offset = +width.slice(0, width.length - 2) * (images.length - 1);
+            slideIndex = images.length;
         } else {
-            offset -= +width.slice(0, width.length - 2)
+            offset -= +width.slice(0, width.length - 2);
+            slideIndex--;
         }
+        updateCurrentSlideNumber();
+        activeDot();
+        slidersField.style.transform = `translateX(-${offset}px)`;
+    });
 
-        if (current.innerHTML == '01') {
-            current.innerHTML = `0${images.length}`
-        } else {
-            current.innerHTML = `0${+current.innerHTML - 1}`
+    // DOTS
+
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+        dots = [];
+    indicators.classList.add('carousel-indicators');
+    slider.append(indicators);
+
+    for (let i = 0; i < images.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.classList.add('dot');
+        if (i == 0) {
+            dot.style.opacity = '1';
         }
+        indicators.append(dot);
+        dots.push(dot);
+    }
 
-        slidersField.style.transform = `translateX(-${offset}px)`
-    })
-})
+    function activeDot() {
+        dots.forEach(dot => dot.style.opacity = '0.5');
+        dots[slideIndex - 1].style.opacity = '1';
+    }
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+            moveToSlide(slideTo);
+        });
+    });
+
+    updateCurrentSlideNumber();
+});
